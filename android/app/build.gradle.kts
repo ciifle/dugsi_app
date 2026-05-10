1b59a7cd1d362a28162bfca8515746d7eeb1f166
ciifle <ciiflecadde07@gmail.com>
2026-05-10 10:49:27 +0300

    updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.util.Properties
+import java.io.FileInputStream
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +9,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
+
+// Load keystore file
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.kobac"
+    namespace = "com.fiddo.dugsi"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kobac"
+        applicationId = "com.fiddo.dugsi"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,17 +43,24 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        debug {
-            // Install alongside release; avoids "failed to install" when old build exists.
-            applicationIdSuffix = ".debug"
-        }
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = file(keystoreProperties["storeFile"] as String?)
+            storePassword = keystoreProperties["storePassword"] as String?
         }
     }
+
+   buildTypes {
+    debug {
+        // Install alongside release; avoids "failed to install" when old build exists.
+        applicationIdSuffix = ".debug"
+    }
+    release {
+    signingConfig = signingConfigs.getByName("release")
+}
+}
 }
 
 flutter {
